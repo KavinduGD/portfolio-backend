4c83cb9971db491fbc346ace0297533c5829bf1d
Kavindu Gihan <kavidudharmasiri90@gmail.com>
2025-10-26 10:00:25 +0530

    index file change

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript + Express!123456");
+  res.send("Hello, TypeScript + Express!12345678");
 });
 
 app.listen(port, () => {
